de11660a1184e44d9d492d5cd0fb3d7b0b14162b
Yami <evila@qq.com>
2018-06-27 03:05:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+var dateObj = new Date();
+var hash = Date.parse(new Date());
+
+function WebpackFileRevPlugin(options) {
+
+}
+
+WebpackFileRevPlugin.prototype.apply = function(compiler) {
+    compiler.plugin('emit', function(compilation, callback) {
+
+        //var dateObj = new Date();
+        var filelist = '// build at ' + dateObj.toLocaleString() + ' \n\n';
+
+        filelist += 'var filelist = [ \n ';
+
+        for (var filename in compilation.assets) {
+            filelist += '\t"' + filename + '",\n';
+        }
+
+        filelist += '];';
+
+        // 将这个列表作为一个新的文件资源，插入到 webpack 构建中：
+        compilation.assets['filelist.js'] = {
+            source: function() {
+                return filelist;
+            },
+            size: function() {
+                return filelist.length;
+            }
+        };
+
+        callback();
+    });
+};
+
+
+module.exports = WebpackFileRevPlugin;
